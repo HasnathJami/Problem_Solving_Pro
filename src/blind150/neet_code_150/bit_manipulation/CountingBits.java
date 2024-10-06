ae3dd9a61e987c15473cdca709ebf18017209bd8
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-10-07 00:47:53 +0600

    Counting Bits (Bit Manipulation)

--- src/blind150/neet_code_150/bit_manipulation/CountingBits.java
+++ src/blind150/neet_code_150/bit_manipulation/CountingBits.java
@@ -3,7 +3,7 @@ package blind150.neet_code_150.bit_manipulation;
 
 
 // Replace class name with "Solution" when submit it to LeetCode
-class NumberOf1Bits {
+class CountingBits {
 
     //Define Approaches
     enum Approach {
@@ -17,26 +17,26 @@ class NumberOf1Bits {
 
 
     // Maintain the actual method name provided by LeetCode to avoid any error during submission time
-    public int hammingWeight(int num) {
+    public int[] countBits(int num) {
         if (approach.equals(Approach.FIRST_APPROACH)) {
             return Approach_1(num);
-        } else if (approach.equals(Approach.SECOND_APPROACH)) {
+        }
+        else if (approach.equals(Approach.SECOND_APPROACH)) {
             return Approach_2(num);
         }
 //        else if (approach.equals(Approach.THIRD_APPROACH)) {
 //            return Approach_3(your_inputs);
 //        }
-        return 0;
+        return null;
     }
 
-    // Approach_1: Bit Manipulation, TC(NumOfSetBits), SC(1)
+    // Approach_1: Bit Manipulation, TC - O(N * NumOfSetBits), SC -> O(N))))
     // Outcome: Outcome of your solution (i.e., ACCEPTED)
     /*
      ---------------------------------------------------------------------- NOTE ----------------------------------------------------------------------
      * Make a short note on your approach
 
       In this problem: Num & (Num-1)  runs NumOfSetBits Times ( the bits are designed in this way when we AND the number and its previous numbers, we can get set bits until the number reaches zero)
-      i.e., 1,2,2 => 1 ^ 2 ^ 2 = 1 (Ans)
     */
 
     /*
@@ -45,17 +45,23 @@ class NumberOf1Bits {
       =>  Rules:
       Run:  num & (num -1 ) until num > 0
     */
-    private int Approach_1(int num) {
-        int countSetBit = 0;
-        while (num > 0) {
-            num = (num & (num - 1));
-            countSetBit++;
+    private int[] Approach_1(int num) {
+        int count[] = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            int countSetBit = 0;
+            int j = i;
+            while (j > 0) {
+                j = (j & (j - 1));
+                countSetBit++;
+            }
+            count[i] = countSetBit;
         }
 
-        return countSetBit;
+
+        return count;
     }
 
-    // Approach_2: Bit Manipulation, TC(NumOfTotalBits), SC(1)
+    // Approach_2: Bit Manipulation, TC -> O(N * NumOfTotalBits), SC -> O(N)
     // Outcome: Outcome of your solution (i.e., ACCEPTED)
     /*
      ---------------------------------------------------------------------- NOTE ----------------------------------------------------------------------
@@ -68,34 +74,40 @@ class NumberOf1Bits {
      ---------------------------------------------------------------------- Algorithm ----------------------------------------------------------------------
      * Write an algorithm on your approach.
       AND Rules:
-      1.( num  & 1) = 1 (lsb is 1 else 0) and the number is odd
+      1.( num  & 1) = 1 (lsb is 1 else 0) (note: the number is odd)
     */
-    private int Approach_2(int num) {
-        int countSetBit = 0;
-        while (num > 0) {
-            if ((num & 1) == 1) {
-                countSetBit++;
+    private int[] Approach_2(int num) {
+        int count[] = new int[num + 1];
+        for (int i = 0; i <= num; i++) {
+            int j = i;
+            int countSetBit = 0;
+            while (j > 0) {
+                if ((j & 1) == 1) {
+                    countSetBit++;
+                }
+                j = j >> 1;
+                count[i] = countSetBit;
             }
-            num = num >> 1;
         }
-
-        return countSetBit;
+        return count;
     }
 
     // Driver Class (Must be excluded in the online judge submission)
     public static class Driver {
         public static void main(String[] args) {
             // Code starts from here.
-            int num = 128;
-            NumberOf1Bits numberOf1Bits = new NumberOf1Bits();
-            int res = numberOf1Bits.hammingWeight(num);
-            System.out.println(res);
+            int num = 4;
+            CountingBits countingBits = new CountingBits();
+            int[] res = countingBits.countBits(num);
+            for (int i = 0; i < res.length; i++) {
+                System.out.print(res[i]);
+            }
 
         }
     }
 }
 
 
-// Conclusion: Approach 1 is Efficient as It only runs NumOfSetBitTimes.
-// Example: 10000000000000000000000000 => Here TC for Approach_1 is 0(1) and Approach_2 is 0(26)
-// FUTURE PLAN:I will implement other approaches
+// Conclusion: Approach 1 is Efficient as It only runs (N * NumOfSetBit) Times.
+// Example: 10000000000000000000000000 => Here TC for Approach_1 is 0(N * 1) and Approach_2 is 0(N * 26)
+// FUTURE PLAN: I will implement other approaches
