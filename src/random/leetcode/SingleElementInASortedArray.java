fc9eba2542be0b60385a8e012daf0bedf9b9fb4b
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-10-12 22:04:16 +0600

    SingleElementInASortedArray (Bit Manipulation)

---
+++ src/random/leetcode/SingleElementInASortedArray.java
@@ -0,0 +1,14 @@
+package random.leetcode;
+
+//Note : This Problem is same as "MISSING NUMBER" Problem
+// Replace class name with "Solution" when submit it to LeetCode
+class SingleElementInASortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int res = 0;
+        for (int i=0;i<nums.length;i++){
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
+
