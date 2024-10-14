098be07598cb2803b265ec93c20f2b6a7ac9a855
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-10-14 08:48:36 +0600

    Find Greatest CommonDivisor Of Array (Euclid Algorithm, Mathematics)

--- src/random/leetcode/mathematics/FindGreatestCommonDivisorOfArray.java
+++ src/random/leetcode/mathematics/FindGreatestCommonDivisorOfArray.java
@@ -30,36 +30,46 @@ class FindGreatestCommonDivisorOfArray {
         return 0;
     }
 
-    // Approach_1: Euclid Theorem,  Time Complexity -> O(nlogn) for (Arrays.sort()) + O(log(min(a,b)) for (GCD calculation) = O(nlogn) ,, and Space Complexity -> O(1)
+    // Approach_1: Euclid Algorithm,  Time Complexity -> O(nlogn) for (Arrays.sort()) + O(log(min(a,b)) for (GCD calculation) = O(nlogn) ,, and Space Complexity -> O(1)
     // Outcome: ACCEPTED
     /*
      ---------------------------------------------------------------------- NOTE ----------------------------------------------------------------------
      * Make a short note on your approach
-
+     1. Here, we have to use Euclid Algorithm to find the GCD of 2 numbers
+     2. In the Euclid Algorithm, if(b<=0) GCD = a else GCD(b, a%b)
+     3. In this formula, here, It tries to perform a%b, and a should be always greater than b (a>b)  always for optimization as it reduce the number of steps
+     4. Suppose, a<b. On the next iteration it's changed to a>b, with this formula (So, we don't need to be worried)
+     5. here, if b == 0, that's means we have found our GCD which we have stored into a, as a = b. every time,
+     and we have performed a%b(if a is divisible by b, then b is our gcd which is stored in variable a)
      */
 
     /*
      ---------------------------------------------------------------------- Algorithm ----------------------------------------------------------------------
      * Write an algorithm on your approach.
-     => Euclid Theorem
+     => Euclid Algorithm
     */
     private int Approach_1(int[] n) {
         // Code starts from here.
         Arrays.sort(n);
-        return calculateGCD(n[0], n[n.length - 1]);
+        return calculateGCD_Recursive(n[0], n[n.length - 1]);
     }
 
 
     public int calculateGCD(int a, int b) {
         while (b > 0) {
-            // int temp = a;
-            // a = b;
-            // b = temp % b;
             int temp = b;
             b = a % b;
             a = temp;
         }
         return a;
+    }
+
+    public int calculateGCD_Recursive(int a, int b) {
+        if (b <= 0) {
+            return a;
+        }
+        return calculateGCD_Recursive(b, a % b);
+
     }
 
 
